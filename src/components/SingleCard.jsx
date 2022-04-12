3364c26c7b6c7d1da9c4ce8939b6a4694bd9d45d
stee1ix <rituraj.ranjan.42@gmail.com>
2022-04-12 12:18:18 +0530

    demo game made from net ninja youtube channek

---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,25 @@
+import './SingleCard.css';
+
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+	const handleClick = () => {
+		if (!disabled) {
+			handleChoice(card);
+		}
+	};
+
+	return (
+		<div className="card">
+			<div className={flipped ? 'flipped' : ''}>
+				<img src={card.src} className="front" alt="card front" />
+				<img
+					src="/img/cover.png"
+					className="back"
+					onClick={handleClick}
+					alt="card back"
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default SingleCard;
